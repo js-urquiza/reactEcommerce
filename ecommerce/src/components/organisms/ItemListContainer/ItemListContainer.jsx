de938937a96d393623421801c57d338fc6aa00a3
juansantiago <js.urquiza@gmail.com>
2023-12-28 17:55:06 +0000

    [ItemListContainer] Agrego un loader a la lista de productos

--- ecommerce/src/components/organisms/ItemListContainer/ItemListContainer.jsx
+++ ecommerce/src/components/organisms/ItemListContainer/ItemListContainer.jsx
@@ -7,19 +7,23 @@ import ItemList from '../../molecules/ItemList/ItemList'
 const ItemListContainer = ({greeting}) => {
 
   const [ products, setProducts ] = useState([])
+  const [ loaded, setLoaded ] = useState(true)
 
   useEffect(() => {
     getProducts().then(res => {
       setProducts(res); // Carga los productos en "products" con nuestra respuesta de la promesa
     })
+    .finally(res => {
+      setLoaded(false);
+    })
   }, [])
   
   return (
     <>
-    <h1>{greeting}</h1>
-    <ItemList products={products} />
+      <h1>{greeting}</h1>
+      {loaded ? <div>Cargando...</div> : <ItemList products={products} />}
     </>
-  )
+  );
 }
 
 export default ItemListContainer
