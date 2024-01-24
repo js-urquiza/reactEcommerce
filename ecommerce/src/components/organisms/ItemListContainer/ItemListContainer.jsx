3a49414a15f0d5806e19ff210bed8839906bbbb4
juansantiago <js.urquiza@gmail.com>
2024-01-24 09:42:28 +0000

    [ItemListContainer] Agrego función fetchProducts que incluye filtro de categoría

--- ecommerce/src/components/organisms/ItemListContainer/ItemListContainer.jsx
+++ ecommerce/src/components/organisms/ItemListContainer/ItemListContainer.jsx
@@ -1,30 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import './ItemListContainer.css'
-import { getProducts } from '../../../database/asyncProductsDB'
+import { getProductByCategory, getProducts } from '../../../database/asyncProductsDB'
 import ItemList from '../../molecules/ItemList/ItemList'
 import Loader from '../../atoms/Loader/Loader'
+import { useParams } from 'react-router-dom'
+import { Category } from '@mui/icons-material'
 
 
 const ItemListContainer = ({greeting}) => {
 
   const [ products, setProducts ] = useState([])
   const [ loaded, setLoaded ] = useState(true)
+  const { category } = useParams()
 
   useEffect(() => {
-    getProducts().then(res => {
-      setProducts(res); // Carga los productos en "products" con nuestra respuesta de la promesa
-    })
-    .finally(res => {
-      setLoaded(false);
-    })
-  }, [])
+    
+    const fetchProducts = async () => {
+      try {
+        setLoaded(true);
+        const result = category ? await getProductByCategory(category) : await getProducts();
+        setProducts(result);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setLoaded(false);
+      }
+    }
+
+    fetchProducts();
+
+  }, [category])
   
   return (
     <>
       <h1>{greeting}</h1>
       {loaded ? <Loader /> : <ItemList products={products} />}
     </>
-  );
+  )
 }
 
 export default ItemListContainer
