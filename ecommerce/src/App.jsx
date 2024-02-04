e120435747ca6531b3d4cb8ba54ab1d8f866bf10
juansantiago <js.urquiza@gmail.com>
2024-02-04 10:35:37 +0000

    [Cart] Creo página carrito

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/organisms/Navbar/Navbar'
 import ItemListContainer from './components/organisms/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from "./components/organisms/ItemDetailContainer/ItemDetailContainer";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Cart from './components/pages/Cart/Cart';
 
 // Contexts
-import { CartContext, CartContextProvider } from './context/CartContext';
+import { CartContextProvider } from './context/CartContext';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
             <Route path='/' element={<ItemListContainer greeting={'Bienvenidos'} />} />
             <Route path='/category/:category' element={<ItemListContainer greeting={'Bienvenidos'} />} />
             <Route path='/item/:id' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart />} />
           </Routes>
         </BrowserRouter>
       </CartContextProvider>
