c8b0eb946ad0857ad5982f6099a7ade2ad2a9d49
juansantiago <js.urquiza@gmail.com>
2024-02-07 21:37:36 +0000

    [ItemListContainer] Cambio función para que consuma Firebase

--- ecommerce/src/components/organisms/ItemListContainer/ItemListContainer.jsx
+++ ecommerce/src/components/organisms/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import './ItemListContainer.css'
-import { getProductByCategory, getProducts } from '../../../database/asyncProductsDB'
+// import { getProductByCategory, getProducts } from '../../../database/asyncProductsDB'
 import ItemList from '../../molecules/ItemList/ItemList'
 import Loader from '../../atoms/Loader/Loader'
 import { useParams } from 'react-router-dom'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { db } from '../../../services/firebaseConfig'
 
 
 const ItemListContainer = ({greeting}) => {
@@ -14,19 +16,43 @@ const ItemListContainer = ({greeting}) => {
 
   useEffect(() => {
     
-    const fetchProducts = async () => {
-      try {
-        setLoaded(true);
-        const result = category ? await getProductByCategory(category) : await getProducts();
-        setProducts(result);
-      } catch (error) {
-        console.log(error.message);
-      } finally {
-        setLoaded(false);
-      }
-    }
+    setLoaded(true);
+    
+    const productsRef = category
+      ? query(collection(db, 'products'), where('category', '==', category)) // Si hay categoría, se usa el método query con el collection de la categoría pasada por parámetro.
+      : collection(db, 'products') // Si no hay categoría, se accede a la colección 'products'.
+        // El resultado se de la colección a usar se asigna a la variable 'productRef'.
 
-    fetchProducts();
+    getDocs(productsRef) // Traemos los documentos de la colección indicada en 'productsRef'.
+      .then(snapshot => {
+        const formatedProducts = snapshot.docs.map(doc => {
+          const data = doc.data(); // Trae los datos de cada producto
+          return {
+            id: doc.id, // Ponemos el id del documento
+            ...data // Agregamos los datos con el spread operatos
+          }
+        })
+        setProducts(formatedProducts); // Seteamos el estado con los nuevos productos formateados con el id.
+      })
+      .finally(() => {
+        setLoaded(false);
+      })
+    
+    // const fetchProducts = async () => {
+    //   try {
+    //     setLoaded(true);
+    //     const result = category
+    //       ? await getProductByCategory(category)
+    //       : await getProducts();
+    //     setProducts(result);
+    //   } catch (error) {
+    //     console.log(error.message);
+    //   } finally {
+    //     setLoaded(false);
+    //   }
+    // }
+
+    // fetchProducts();
 
   }, [category])
   
